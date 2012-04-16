cc0ee0f388cb315383ed26da0e2af2cac1c10ff9
Tamber Penketh <tamber@furryhelix.co.uk>
2012-04-16 22:24:19 +0100

    Document that I'm reserving IRQs up to 16.

--- src/include/irq.h
+++ src/include/irq.h
@@ -9,6 +9,9 @@ enum {
      IRQ_STACK_OVER,
      IRQ_STACK_UNDER,
      IRQ_MCE
+     /* Interrupts 6 to 16 are reserved for internal use. 17 to
+      * (2^word-width)-1, however, are available for software and
+      * hardware interrupts.*/
 };
 
 void interrupt(muword irq);
